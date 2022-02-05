cfb2672716c0387fa23f4fd783da6b89352379b4
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-05 18:53:51 -0500

    improved layout

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import Github from './social/Github';
-import Youtube from './social/Youtube';
-import Instagram from './social/Instagram';
-import Twitter from './social/Twitter';
+import SocialCard from './social/SocialCard';
+import { faTwitter, faGithub, faYoutube, faInstagram, faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Social = () => {
   return (
     <div id="social">
       <h2>Find me on the interwebs</h2>
-      <div className="flex flex-row flex-wrap justify-center">
-        <Twitter />
-        <Github />
-        <Instagram />
-        <Youtube />
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 sm:mx-36">
+        <SocialCard title="Twitter" text="A collection of my latest shower thoughts" cta="Follow" href="https://twitter.com/flyme2bluemoon" icon={faTwitter} />
+        <SocialCard title="Github" text="A place for me to show off my code" cta="Visit" href="https://github.com/flyme2bluemoon" icon={faGithub} />
+        <SocialCard title="Youtube" text="A chronicle of my latest travel, before the pandemic at least" cta="Watch" href="https://www.youtube.com/channel/UCbOygd7cfMtsl3nKCJP_8Kg" icon={faYoutube} />
+        <SocialCard title="Instagram" text="An assortment of way to many plane pictures" cta="Follow" href="https://instagram.com/flyme2bluemoon" icon={faInstagram} />
+        <SocialCard title="Email" text="A method to contact me" cta="Contact" href="mailto:335562906@gapps.yrdsb.ca" icon={faEnvelope} />
+        <SocialCard title="Twitch" text="A website where I occasionally stream to" cta="Watch" href="https://twitch.tv/flyme2bluemoon" icon={faTwitch} />
       </div>
     </div>
   );
---
+++ src/components/types/Card.ts
@@ -0,0 +1,11 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+type SocialCardProps = {
+  title: string,
+  text: string,
+  href: string,
+  cta: string,
+  icon: IconDefinition
+}
+
+export type { SocialCardProps };
