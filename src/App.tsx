385052cb114d824bd566b5ed03902f932a232c28
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-04 20:44:37 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import 'flowbite';
 import './App.css';
+import Navbar from './components/Navbar';
+import Social from './components/Social';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Navbar />
+      <div className="flex p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800 m-5" role="alert">
+        <svg className="inline flex-shrink-0 mr-3 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+        <div>
+          <span className="font-medium">Warning:</span>
+          This page is still under development. <a href="https://github.com/flyme2bluemoon/flyme2bluemoon.github.io/issues" className="underline">Click here to submit any feedback!</a>
+        </div>
+      </div>
+      <h1>Hello, I'm Matthew Shen 👋</h1>
+      <Social />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Social.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Github from './social/Github';
+import Youtube from './social/Youtube';
+import Instagram from './social/Instagram';
+import Twitter from './social/Twitter';
+
+const Social = () => {
+  return (
+    <div id="social">
+      <h2>Find me on the interwebs</h2>
+      <div className="flex flex-row flex-wrap justify-center">
+        <Twitter />
+        <Github />
+        <Instagram />
+        <Youtube />
+      </div>
+    </div>
+  );
+}
+
+export default Social;
