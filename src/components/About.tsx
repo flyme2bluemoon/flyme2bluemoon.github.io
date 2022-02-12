d6c1db964c3eb154fff15891107e2c70df07c86c
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-12 17:02:26 -0500

    smol update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className="mt-24">
       <span id="about" className="block relative -top-10" />
       <h2 className="h2 text-center">About Me</h2>
       <div className="mx-auto w-3/4 p-6 border-2 rounded-lg border-slate-300 dark:border-slate-600 bg-slate-100/90 dark:bg-slate-900/90 shadow-xl font-medium">
