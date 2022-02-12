00ac7c94166a0a3e5396c2a7041e349e5a3826d4
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-12 13:41:12 -0500

    smol change to test gh-actions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <span id="about" className="block relative -top-10" />
       <h2 className="h2 text-center">About Me</h2>
       <div className="mx-auto w-3/4 p-6 border-2 rounded-lg border-slate-300 dark:border-slate-600 bg-slate-100/90 dark:bg-slate-900/90 shadow-xl font-medium">
-        Hey there!
+        Hi there!
         <br />
         <br />
         <ul className="list-disc px-4">
