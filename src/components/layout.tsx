013231c84ae7e45249dc00bfd671e25a987a8413
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-26 16:46:07 -0500

    improve lighthouse score

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ import { Helmet } from "react-helmet";
 const Layout = ({ pageTitle, children }: { pageTitle: string, children: React.ReactElement }) => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
-      <Helmet>
+      <Helmet htmlAttributes={{ lang: "en" }}>
         <title>{pageTitle} | flyme2bluemoon</title>
         <meta name="google-site-verification" content="qv7xCfbt8MhWrx_-4gE7-b9MwPs0qsiPhjMu46L1Y18" />
         <meta http-equiv="content-language" content="en-us" />
