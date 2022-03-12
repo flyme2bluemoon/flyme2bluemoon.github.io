af0e7bab1d31f60a5fc565548f35fbd7ea42e680
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-03-12 13:25:14 -0500

    added gh stats page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,6 +30,9 @@ const Navbar = () => {
               <Link to="/projects" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</Link>
             </li>
             <li>
+              <Link to="/stats" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Stats</Link>
+            </li>
+            <li>
               <Link to="/contact" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
             </li>
           </ul>
---
+++ src/pages/stats.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { getRepos } from "../../utils/codeGallery";
+import Layout from "../components/layout";
+import * as colors from "../../utils/colors.json";
+
+const Stats = () => {
+  const [languages, setLanguages] = useState<any>({});
+  
+  const getStats = async () => {
+    const repos = await getRepos();
+    let histogram: any = {};
+    for (let i = 0; i < repos.length; i++) {
+      if (repos[i].language !== null) {
+        histogram[repos[i].language] = (histogram[repos[i].language] ? histogram[repos[i].language] + 1 : 1);
+      }
+    }
+    setLanguages(histogram);
+  }
+
+  useEffect(() => {
+    getStats();
+  }, []);
+
+  return (
+    <Layout pageTitle="Stats">
+      <div className="max-w-[1280px] mx-auto">
+        <h1 className="text-6xl font-bold py-6">My Github Stats</h1>
+        <div>
+          <h2 className="text-4xl font-semibold pb-6">Most Used Languages</h2>
+          <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 flex flex-row">
+            {Object.keys(languages).map((lang) => {
+              // @ts-expect-error
+              const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
+              const count = Object.keys(languages).indexOf(lang);
+              const total = Object.keys(languages).length - 1;
+              return (
+                <div key={lang} className={`h-2.5 ${count === 0 ? "rounded-l" : ""} ${count === total ? "rounded-r" : ""}`} style={{
+                  width: `${Math.round(languages[lang] / sum * 100)}%`,
+                  // @ts-expect-error
+                  backgroundColor: colors[lang].color
+                }}></div>
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-3">
+            {Object.keys(languages).map((lang) => {
+              // @ts-expect-error
+              const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
+              return (
+                // @ts-expect-error
+                <div className="w-[80%] mx-auto py-4"><span style={{color: colors[lang].color}}>&#9679;</span> {lang} {(languages[lang] / sum * 100).toFixed(2)}%</div>
+              );
+            })}
+          </div>
+          <small className="text-gray-500 dark:text-gray-400">Data from Github API based on most used language in each public repo</small>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Stats;
