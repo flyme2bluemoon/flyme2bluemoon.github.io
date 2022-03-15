ed1bbab9796dafd2cf4971839b85619ed48d623a
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-03-15 12:35:08 -0400

    updated styles

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -34,33 +34,36 @@ const Stats = () => {
       <div className="max-w-[1280px] mx-auto">
         <h1 className="text-6xl font-bold py-6">My Github Stats</h1>
         <div>
-          <h2 className="text-4xl font-semibold pb-6">Most Used Languages</h2>
-          <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 flex flex-row">
-            {Object.keys(languages).map((lang) => {
-              // @ts-expect-error
-              const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
-              const count = Object.keys(languages).indexOf(lang);
-              const total = Object.keys(languages).length - 1;
-              return (
-                <div key={lang} className={`h-2.5 ${count === 0 ? "rounded-l" : ""} ${count === total ? "rounded-r" : ""}`} style={{
-                  width: `${languages[lang] / sum * 100}%`,
-                  // @ts-expect-error
-                  backgroundColor: colors[lang].color
-                }}></div>
-              );
-            })}
-          </div>
-          <div className="grid grid-cols-3">
-            {Object.keys(languages).map((lang) => {
-              // @ts-expect-error
-              const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
-              return (
+          <div className="rounded-xl bg-gradient-to-r bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 p-2 sm:p-6">
+            <h2 className="text-4xl font-semibold">Most Used Languages</h2>
+            <hr className="my-6 border-gray-500 dark:border-gray-400" />
+            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 flex flex-row">
+              {Object.keys(languages).map((lang) => {
                 // @ts-expect-error
-                <div key={lang} className="w-[80%] mx-auto py-4"><span style={{color: colors[lang].color}}>&#9679;</span> <span className="font-semibold">{lang}</span> {(languages[lang] / sum * 100).toFixed(2)}%</div>
-              );
-            })}
+                const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
+                const count = Object.keys(languages).indexOf(lang);
+                const total = Object.keys(languages).length - 1;
+                return (
+                  <div key={lang} className={`h-2.5 ${count === 0 ? "rounded-l" : ""} ${count === total ? "rounded-r" : ""}`} style={{
+                    width: `${languages[lang] / sum * 100}%`,
+                    // @ts-expect-error
+                    backgroundColor: colors[lang].color
+                  }}></div>
+                );
+              })}
+            </div>
+            <div className="grid grid-cols-3">
+              {Object.keys(languages).map((lang) => {
+                // @ts-expect-error
+                const sum: number = Object.values(languages).reduce((sum, next) => (sum + next), 0);
+                return (
+                  // @ts-expect-error
+                  <div key={lang} className="w-[80%] mx-auto py-4"><span style={{color: colors[lang].color}}>&#9679;</span> <span className="font-semibold">{lang}</span> {(languages[lang] / sum * 100).toFixed(2)}%</div>
+                );
+              })}
+            </div>
+            <small className="text-gray-500 dark:text-gray-400">Data from Github API based on most used language in each public repo</small>
           </div>
-          <small className="text-gray-500 dark:text-gray-400">Data from Github API based on most used language in each public repo</small>
         </div>
       </div>
     </Layout>
