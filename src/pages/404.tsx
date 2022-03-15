96d090cd95a9628d192da499845536811eeaa832
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-03-15 12:15:15 -0400

    improve ux on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,10 +5,10 @@ import Layout from "../components/layout";
 const NotFoundPage = () => {
   return (
     <Layout pageTitle="404 Not Found">
-      <div className="min-h-[80vh] w-[1000px] mx-auto">
+      <div className="w-[1000px] mx-auto">
         <h1 className="text-3xl p-10"><span className="font-bold">404.</span> This is awkward!</h1>
-        <p className="text-xl pt-10">Sorry, the page you requested could not be found. Double check that you have typed or pasted the correct URL. Otherwise, the page may have been moved or deleted.</p>
-        <p className="text-xl pt-10"><Link to="/">Follow the North Star Home</Link></p>
+        <p className="text-xl">Sorry, the page you requested could not be found. Double check that you have typed or pasted the correct URL. Otherwise, the page may have been moved or deleted.</p>
+        <p className="text-xl pt-10"><Link to="/" className="underline">Follow the North Star Home</Link></p>
       </div>
     </Layout>
   );
