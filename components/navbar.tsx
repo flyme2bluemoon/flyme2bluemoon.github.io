ce104ba54d0b3dae0e791c05f8ebb19ed6380a20
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2024-04-20 14:05:46 -0400

    updated navbar - refactoring - added Lecture Notes link - fixed hamburger menu layout on mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,9 +9,17 @@ const Navbar = () => {
 
   const setTheme = useDarkMode();
 
+  const links = {
+    "About": "/about/",
+    "Blog": "/blog/",
+    "Projects": "/projects/",
+    "Lecture Notes": "https://cs.mcgill.ca/~mshen/lecture-notes/",
+    "Stats": "/stats/",
+  };
+
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 rounded-b dark:bg-gray-800 shadow-lg">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
+    <nav className="rounded-b bg-white px-2 py-2.5 shadow-lg sm:px-4 dark:bg-gray-800">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
         <Link href="/" className="flex">
           <img
             src="/images/logo.png"
@@ -20,90 +28,35 @@ const Navbar = () => {
             height="40px"
             alt=""
           />
-          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center whitespace-nowrap text-lg font-semibold dark:text-white">
             Matthew Shen
           </span>
         </Link>
-        <div className="flex-grow"></div>
-        <button
-          id=""
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          onClick={() => {
-            setMenuExpanded(!menuExpanded);
-          }}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <svg
-            className="hidden w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
+        <div className="flex-grow" />
+
         <div
           className={`${
             menuExpanded ? "" : "hidden"
-          } w-full md:block md:w-auto`}
+          } order-last w-full md:order-none md:block md:w-auto`}
           id="mobile-menu"
         >
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <Link
-                href="/about/"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/blog/"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/projects/"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/stats/"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Stats
-              </Link>
-            </li>
+          <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+            {Object.entries(links).map(([text, href]) => {
+              return (
+                <li key={text}>
+                  <Link
+                    href={href}
+                    className="block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  >
+                    {text}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <button
-          className={`theme-toggle [-webkit-tap-highlight-color:transparent] ${
-            menuExpanded ? "invisible md:visible" : ""
-          } p-1 bg-none border-0 w-8 h-8 aspect-square rounded-md cursor-pointer touch-manipulation outline-offset-[5px] hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 ml-2 md:ml-6`}
+          className="theme-toggle ml-2 aspect-square h-8 w-8 cursor-pointer touch-manipulation rounded-md border-0 bg-none p-1 outline-offset-[5px] [-webkit-tap-highlight-color:transparent] hover:bg-gray-100 focus:outline-none md:ml-6 dark:text-gray-400 dark:hover:bg-gray-700"
           id="theme-toggle"
           title="Toggles light and dark theme"
           aria-label="auto"
@@ -112,12 +65,12 @@ const Navbar = () => {
             setTheme(
               document.documentElement.classList.contains("dark")
                 ? "light"
-                : "dark"
+                : "dark",
             );
           }}
         >
           <svg
-            className="group sun-and-moon w-full h-full"
+            className="sun-and-moon group h-full w-full"
             aria-hidden="true"
             width="2"
             height="24"
@@ -125,7 +78,7 @@ const Navbar = () => {
             strokeLinecap="round"
           >
             <circle
-              className="sun transition-transform duration-500 ease-in-out dark:duration-250 origin-center fill-gray-700 group-hover:fill-gray-900 dark:fill-gray-300 dark:group-hover:fill-gray-100 dark:scale-[175%]"
+              className="sun dark:duration-250 origin-center fill-gray-700 transition-transform duration-500 ease-in-out group-hover:fill-gray-900 dark:scale-[175%] dark:fill-gray-300 dark:group-hover:fill-gray-100"
               cx="12"
               cy="12"
               r="6"
@@ -133,7 +86,7 @@ const Navbar = () => {
               fill="currentColor"
             />
             <g
-              className="sun-beams transition-[transform,_opacity] duration-500 ease-in-out dark:duration-250 dark:-rotate-[25deg] origin-center stroke-gray-700 group-hover:stroke-gray-900 dark:opacity-0"
+              className="sun-beams dark:duration-250 origin-center stroke-gray-700 transition-[transform,_opacity] duration-500 ease-in-out group-hover:stroke-gray-900 dark:-rotate-[25deg] dark:opacity-0"
               stroke="currentColor"
               strokeWidth="2px"
             >
@@ -149,13 +102,47 @@ const Navbar = () => {
             <mask className="moon origin-center" id="moon-mask">
               <rect x="0" y="0" width="100%" height="100%" fill="white" />
               <circle
-                className="transition-[transform,_cx] duration-250 ease-in-out dark:delay-250 dark:duration-500 dark:-translate-x-[7px]"
+                className="duration-250 dark:delay-250 transition-[transform,_cx] ease-in-out dark:-translate-x-[7px] dark:duration-500"
                 cx="24"
                 cy="10"
                 r="6"
                 fill="black"
               />
             </mask>
+          </svg>
+        </button>
+        <button
+          id=""
+          type="button"
+          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          onClick={() => {
+            setMenuExpanded(!menuExpanded);
+          }}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-6 w-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden h-6 w-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
           </svg>
         </button>
       </div>
